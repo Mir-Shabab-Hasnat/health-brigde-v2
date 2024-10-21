90a5aebe3a7de8c9845edb3912eeedd91daf11a2
unknown <shababhasnat@gmail.com>
2024-10-21 00:33:09 -0600

    navbar added

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import React, { ReactNode } from "react";
 const layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center">
-      <div className="flex bg-indigo-900 py-3 px-9 rounded-3xl"> <Logo /></div>
+       <Logo />
        
       <div className="mt-12">{children}</div>
     </div>
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -6,12 +6,23 @@ const Logo = () => {
     <a href="/" className="flex items-center gap-2">
       
         <Rainbow className="stroke h-11 w-11 stroke-indigo-500 stroke=[1.5] mr-4" />
-        <p className="bg-gradient-to-r from-indigo-50 to-indigo-300 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        <p className="bg-gradient-to-r from-indigo-400 to-indigo-600 dark:from-indigo-50 dark:to-indigo-300 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
           HealthBridge
         </p>
       
     </a>
   );
 };
+
+export const LogoMobile = () => {
+  return (
+    <a href='/' className='flex items-center gap-2'>
+        
+        <p className='bg-gradient-to-r from-indigo-400 to-indigo-600 dark:from-indigo-50 dark:to-indigo-300 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>
+            HealthBridge
+        </p>
+    </a>
+  )
+}
 
 export default Logo;
