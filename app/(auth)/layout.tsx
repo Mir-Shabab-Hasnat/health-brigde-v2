5292601d755b85731ff008e50b8beca448c4abb9
unknown <shababhasnat@gmail.com>
2024-10-21 00:22:27 -0600

    theme changes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,11 @@
-import Logo from "@/components/logo";
+import Logo from "@/components/Logo";
 import React, { ReactNode } from "react";
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center">
-        <Logo />
+      <div className="flex bg-indigo-900 py-3 px-9 rounded-3xl"> <Logo /></div>
+       
       <div className="mt-12">{children}</div>
     </div>
   );
