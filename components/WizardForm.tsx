af9feb7d5531f99c0e2655f75547df14c5f8829e
unknown <shababhasnat@gmail.com>
2024-11-02 21:13:31 -0600

    mutation and form working correctly

--- components/WizardForm.tsx
+++ components/WizardForm.tsx
@@ -13,8 +13,13 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import SkeletonWrapper from "./SkeletonWrapper";
+import { User } from "@prisma/client";
+import { useEffect, useState } from "react";
+import { UpdateUserInfo } from "@/app/wizard/_actions/userInfo";
+import { toast } from "sonner";
+import { redirect, useRouter } from "next/navigation";
 
 const phoneNumberRegex = /^\+?[1-9]\d{1,14}$/;
 
@@ -22,17 +27,40 @@ const formSchema = z.object({
   phoneNumber: z
     .string()
     .nonempty("Phone number is required")
-    .regex(phoneNumberRegex),
-  location: z.string(),
+    .regex(phoneNumberRegex)
+    .min(10),
+  location: z.string().nonempty("location is required"),
 });
 
 const WizardForm = () => {
-  const userInfo = useQuery({
+  //const [phoneNumber, setPhoneNumber] = useState<string | null>(null)
+  const router = useRouter()
+
+
+  const userInfo = useQuery<User>({
     queryKey: ["userInfo"],
     queryFn: () => fetch("/api/user-info").then((res) => res.json()),
   });
 
   //console.log("@@@ USER INFO", userInfo)
+
+  const mutation = useMutation({
+    mutationFn: UpdateUserInfo,
+    onMutate: () => {
+      toast.loading("Updating user info...")
+    },
+    onSuccess: (data: User) => {
+      // Handle success, e.g., show a success message or refetch user info
+      toast.dismiss()
+      toast.success("User info updated successfully")
+      router.push("/")
+    },
+    onError: (error) => {
+      // Handle error, e.g., show an error message
+      toast.dismiss()
+      toast.error("Error updating user info:" + error);
+    },
+  })
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -42,7 +70,9 @@ const WizardForm = () => {
     },
   });
 
-  const handleSubmit = () => {};
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    mutation.mutate(data)
+  };
   return (
     <SkeletonWrapper isLoading={userInfo.isFetching}>
       <div className="mt-4">
@@ -82,7 +112,7 @@ const WizardForm = () => {
                 );
               }}
             />
-            <Button type="submit" className="w-full">
+            <Button type="submit" className="w-full" disabled={mutation.isPending}>
               Submit
             </Button>
           </form>
---
+++ schema/userInfo.ts
@@ -0,0 +1,12 @@
+import {z} from "zod"
+
+const phoneNumberRegex = /^\+?[1-9]\d{1,14}$/;
+
+export const UpdateUserInfoSchema = z.object({
+    phoneNumber: z
+    .string()
+    .nonempty("Phone number is required")
+    .regex(phoneNumberRegex)
+    .min(10),
+  location: z.string(),
+});
