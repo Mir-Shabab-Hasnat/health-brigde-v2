0a0d849c35a0fe396076d4d666bf4e2dcd33bb34
unknown <shababhasnat@gmail.com>
2024-11-02 18:29:28 -0600

    wizard page started

--- app/wizard/layout.tsx
+++ app/wizard/layout.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react'
 
 const layout = ({children}: {children: ReactNode}) => {
   return (
-    <div className='relative flex h-screen w-full flex-col items-start justify-center'>
+    <div className='relative flex h-screen w-full flex-col items-center justify-center p-6'>
         {children}
     </div>
   )
--- app/wizard/page.tsx
+++ app/wizard/page.tsx
@@ -1,17 +1,45 @@
-import { currentUser } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation';
-import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
 
-export const page = async () => {
-    const user = await currentUser();
+const page = async () => {
+  const user = await currentUser();
 
-    if (!user) {
-        redirect("/sign-in")
-    }
+  if (!user) {
+    redirect("/sign-in");
+  }
 
   return (
-    <div className='container flex max-w-2xl flex-col items-center justify-between gap-4'>
+    <div className="container flex max-w-2xl flex-col items-center justify-between gap-4">
+      <div>
+        <h1 className="text-center text-3xl">
+          Welcome, <span className="ml-2 font-bold">{user.firstName}</span>
+        </h1>
+        <h2 className="mt-4 text-center text-base text-muted-foreground">
+          Please let us know your personal details
+        </h2>
+      </div>
 
+      <Separator />
+
+      <Card className="w-full">
+        <CardHeader>
+            <CardTitle>
+                Personal Info
+            </CardTitle>
+            <CardDescription>
+                Give us some of your vital information
+            </CardDescription>
+            <CardContent>
+                
+            </CardContent>
+        </CardHeader>
+
+      </Card>
     </div>
-  )
-}
+  );
+};
+
+export default page;
