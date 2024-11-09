1bf3ca4289f37eba831ef3ed1a0cb534446d431d
unknown <shababhasnat@gmail.com>
2024-11-09 13:18:39 -0600

    skeleton for form created

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
@@ -23,7 +24,24 @@ const page = async () => {
   
   
 
-  return <div>page</div>;
+  return (
+    <div className="h-full bg-background">
+        <div className="border-b bg-card">
+          <div className="container mx-auto flex flex-wrap items-center justify-between gap-6 px-8 py-8">
+            <p className="text-3xl font-bold">
+              Hello, {user.firstName}!
+            </p>
+
+            <div className="flex items-center gap-3">
+              <Button variant={"outline"} className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white">
+                Make an Appointment Application
+              </Button>
+            </div>
+          </div>
+
+        </div>
+    </div>
+  );
 };
 
 export default page;
---
+++ app/appointment-form/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className='two-column-layout flex min-h-screen'>
+        <div className='form-container'>
+            form contents
+        </div>
+        <div className='chat-container'>
+            chat contents
+        </div>
+    </main>
+  )
+}
+
+export default page
