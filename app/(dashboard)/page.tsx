c8a976312f8520593ae2524b5267141ba3209418
unknown <shababhasnat@gmail.com>
2024-11-09 12:38:13 -0600

    wizard routing working correctly

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+const page = async () => {
+  const user = await currentUser();
 
-export default page
+  if (!user) {
+    redirect("/sign-in")
+  }
+
+  const userSettings = await prisma.user.findUnique({
+    where: {
+      userId: user.id
+    }
+  })
+
+  if (!userSettings) {
+    redirect("/wizard")
+  }
+  
+  
+  
+
+  return <div>page</div>;
+};
+
+export default page;
